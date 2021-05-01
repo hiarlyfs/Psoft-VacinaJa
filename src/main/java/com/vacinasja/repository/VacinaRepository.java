34f33e565d30ceda3922805f63be53f597a29a2a
lucasgsa <lucasgsalmeida19@gmail.com>
2021-05-01 18:38:19 -0300

    adicionando repository de Vacina

---
+++ src/main/java/com/vacinasja/repository/VacinaRepository.java
@@ -0,0 +1,9 @@
+package com.vacinasja.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.vacinasja.model.Vacina;
+
+public interface VacinaRepository extends JpaRepository<Vacina, Long>{
+
+}
