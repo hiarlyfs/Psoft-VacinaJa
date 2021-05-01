1afecce7c05ef8682c599c1094eda9c94a5302cf
lucasgsa <lucasgsalmeida19@gmail.com>
2021-05-01 18:37:27 -0300

    criando entity LoteVacina

---
+++ src/main/java/com/vacinasja/model/LoteVacina.java
@@ -0,0 +1,57 @@
+package com.vacinasja.model;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import io.micrometer.core.lang.NonNull;
+
+@Entity
+public class LoteVacina {
+	
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+	
+	@NonNull
+    @Temporal(TemporalType.DATE)
+    private Date dataValidade;
+
+    @NonNull
+	private Integer qtdDoses;
+    
+    @ManyToOne
+    @JoinColumn(name = "vacina_id")
+    private Vacina vacina;
+    
+    public LoteVacina() {
+	}
+
+	public LoteVacina(Date dataValidade, Integer qtdDoses, Vacina vacina) {
+		super();
+		this.dataValidade = dataValidade;
+		this.qtdDoses = qtdDoses;
+		this.vacina = vacina;
+	}
+	
+    public Date getDataValidade() {
+		return dataValidade;
+	}
+
+	public Integer getQtdDoses() {
+		return qtdDoses;
+	}
+
+	public Vacina getTipoVacina() {
+		return vacina;
+	}
+    
+    
+}
