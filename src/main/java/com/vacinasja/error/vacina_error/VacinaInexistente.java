64085c7d01b8f006364792ab2d43b8fc9264af38
lucasgsa <lucasgsalmeida19@gmail.com>
2021-05-01 18:37:03 -0300

    adicionando erro para vacina inexistente

---
+++ src/main/java/com/vacinasja/error/vacina_error/VacinaInexistente.java
@@ -0,0 +1,13 @@
+package com.vacinasja.error.vacina_error;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.NOT_ACCEPTABLE)
+public class VacinaInexistente extends Exception {
+    static final String VACINA_INEXISTENTE = "O id %s de vacina não está cadastrado.";
+
+    public VacinaInexistente(Long vacinaId) {
+        super(String.format(VACINA_INEXISTENTE, vacinaId));
+    }
+}
