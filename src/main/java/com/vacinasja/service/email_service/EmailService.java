ab1398eb44a1119dd83877d704beb1d5870d3c0d
lucasgsa <lucasgsalmeida19@gmail.com>
2021-05-05 09:30:17 -0300

    divindo em uma função genêrica e envio de emails, e uma especifica do template

--- src/main/java/com/vacinasja/service/email_service/EmailService.java
+++ src/main/java/com/vacinasja/service/email_service/EmailService.java
@@ -5,5 +5,6 @@ import java.io.IOException;
 import javax.mail.MessagingException;
 
 public interface EmailService {
+	public void enviarEmail(String emailDestinatario, String assuntoEmail, String mensagemEmail) throws MessagingException;
 	public void enviarEmailDefault(String emailDestinatario, String nomeDestinatario, String assuntoEmail, String mensagemEmail) throws MessagingException, IOException;
 }
--- src/main/java/com/vacinasja/service/email_service/EmailServiceImpl.java
+++ src/main/java/com/vacinasja/service/email_service/EmailServiceImpl.java
@@ -12,7 +12,6 @@ import javax.mail.internet.MimeMessage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
-import org.springframework.core.io.ResourceLoader;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
@@ -25,25 +24,38 @@ public class EmailServiceImpl implements EmailService {
     @Autowired
     private JavaMailSender emailSender;
     
-    @Autowired
-    ResourceLoader resourceLoader;
+    private final String defaultEmailTemplate = "templates/default_email.html";
     
-    public void enviarEmailDefault(
-      String emailDestinatario, String nomeDestinatario, String assuntoEmail, String mensagemEmail) throws MessagingException, IOException {
+    @Override
+    public void enviarEmail(String emailDestinatario, String assuntoEmail, String mensagemEmail) throws MessagingException {
     	MimeMessage message = emailSender.createMimeMessage();
-    	
     	MimeMessageHelper helper = new MimeMessageHelper(message, "UTF-8");
+    	
     	helper.setTo(emailDestinatario);
     	helper.setSubject(assuntoEmail);
-    	String text_template = getDefaultTemplate();
-    	String replaced = text_template.replaceAll("#nome", nomeDestinatario)
-    			.replaceAll("#mensagem", mensagemEmail);
-    	helper.setText(replaced, true);
+    	helper.setText(mensagemEmail, true);
         emailSender.send(message);
     }
     
+    /**
+     * Envia um e-mail utilizando o template default.
+     */
+    @Override
+    public void enviarEmailDefault(String emailDestinatario, String nomeDestinatario, String assuntoEmail, String mensagemEmail) throws MessagingException, IOException {
+    	String text_template = getDefaultTemplate();
+    	String replaced = text_template
+    			.replaceAll("#nome", nomeDestinatario)
+    			.replaceAll("#mensagem", mensagemEmail);
+    	enviarEmail(emailDestinatario, assuntoEmail, replaced);
+    }
+    
+    /**
+     * Recupera o arquivo de template default e retorna como String.
+     * @return
+     * @throws IOException
+     */
     private String getDefaultTemplate() throws IOException {
-    	Resource resource = new ClassPathResource("templates/default_email.html");
+    	Resource resource = new ClassPathResource(defaultEmailTemplate);
     	InputStream inputStream = new ByteArrayInputStream(resource.getInputStream().readAllBytes());
     	
         String text = null;
