ff602363912b67eb0fe138301929b959c2174272
lucasgsa <lucasgsalmeida19@gmail.com>
2021-05-04 22:41:33 -0300

    modificando para @Service

--- src/main/java/com/vacinasja/service/email_service/EmailServiceImpl.java
+++ src/main/java/com/vacinasja/service/email_service/EmailServiceImpl.java
@@ -3,9 +3,9 @@ package com.vacinasja.service.email_service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
-@Component
+@Service
 public class EmailServiceImpl implements EmailService {
 	
     @Autowired
