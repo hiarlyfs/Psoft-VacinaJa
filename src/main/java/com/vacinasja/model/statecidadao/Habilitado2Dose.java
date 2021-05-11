657e3f12f3c24e75b11669cae6ad48615a18a996
Pedro D <pedroxcg@gmail.com>
2021-05-11 14:02:21 -0300

    Criado estágios de vacinação do Cidadão

---
+++ src/main/java/com/vacinasja/model/statecidadao/Habilitado2Dose.java
@@ -0,0 +1,29 @@
+package com.vacinasja.model.statecidadao;
+
+import com.vacinasja.model.Cidadao;
+import com.vacinasja.model.Vacina;
+
+import javax.persistence.Entity;
+import java.time.LocalDate;
+
+@Entity
+public class Habilitado2Dose extends Estado {
+
+    @Override
+    public void tentaAlterar(Cidadao cidadao, Integer idade, String profissao, String comorbidade) { // faz nada, pois já está habilitado.
+    }
+
+    @Override
+    public void tentaAlterar(Cidadao cidadao, LocalDate data) { // faz nada, pois já tomou a primeira dose.
+    }
+
+    @Override
+    public void tentaAlterar(Cidadao cidadao, Vacina vacina) {
+        cidadao.setNewEstagioVacinacao(new Vacinado());
+    }
+
+    @Override
+    public String toString() {
+        return "Habilitado para tomar a 2o dose";
+    }
+}
---
+++ src/main/java/com/vacinasja/model/statecidadao/Vacinado.java
@@ -0,0 +1,28 @@
+package com.vacinasja.model.statecidadao;
+
+import com.vacinasja.model.Cidadao;
+import com.vacinasja.model.Vacina;
+
+import javax.persistence.Entity;
+import java.time.LocalDate;
+
+@Entity
+public class Vacinado extends Estado {
+
+    @Override
+    public void tentaAlterar(Cidadao cidadao, Integer idade, String profissao, String comorbidade) { // faz nada, já está vacinado.
+    }
+
+    @Override
+    public void tentaAlterar(Cidadao cidadao, LocalDate data) { // faz nada, já está vacinado.
+    }
+
+    @Override
+    public void tentaAlterar(Cidadao cidadao, Vacina vacina) { // faz nada, já está vacinado.
+    }
+
+    @Override
+    public String toString() {
+        return "Finalizada vacinação";
+    }
+}
