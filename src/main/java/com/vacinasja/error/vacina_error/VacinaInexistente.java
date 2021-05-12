a36ced26d7dcc2a4599f2273dffd15b32cbbca9f
lucasgsa <lucasgsalmeida19@gmail.com>
2021-05-12 13:09:41 -0300

    modificando erro de retorno para 404(NOT_FOUND)

--- src/main/java/com/vacinasja/error/vacina_error/VacinaInexistente.java
+++ src/main/java/com/vacinasja/error/vacina_error/VacinaInexistente.java
@@ -3,7 +3,7 @@ package com.vacinasja.error.vacina_error;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(code = HttpStatus.NOT_ACCEPTABLE)
+@ResponseStatus(code = HttpStatus.NOT_FOUND)
 public class VacinaInexistente extends Exception {
     static final String VACINA_INEXISTENTE = "O id %s de vacina não está cadastrado.";
 
