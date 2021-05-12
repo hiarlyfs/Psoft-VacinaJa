5ecd434a28dba2dc3af5079db5afca3e2affbf01
Pedro D <pedroxcg@gmail.com>
2021-05-12 11:11:29 -0300

    Adicionado atualização dos dados de Cidadão

--- src/main/java/com/vacinasja/controller/CidadaoController.java
+++ src/main/java/com/vacinasja/controller/CidadaoController.java
@@ -1,8 +1,10 @@
 package com.vacinasja.controller;
 
 import com.vacinasja.dto.cidadao.InsertCidadaoDto;
+import com.vacinasja.dto.cidadao.UpdateCidadaoDto;
 import com.vacinasja.error.cidadao_error.CidadaoNaoEncontrado;
 import com.vacinasja.error.tipo_login_error.TipoLoginInvalido;
+import com.vacinasja.model.Cidadao;
 import com.vacinasja.model.LoginCidadao;
 import com.vacinasja.service.cidadao_service.CidadaoService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,10 +29,24 @@ public class CidadaoController {
         return new ResponseEntity<>(loginCidadao, HttpStatus.OK);
     }
 
-    @GetMapping("/")
+    @GetMapping("/estagio")
     public ResponseEntity<String> listaEstagioCidadao(@RequestParam String cpf) throws CidadaoNaoEncontrado {
         // atualizar para pegar informação de usuário logado da sessão
         String estagio = cidadaoService.listaEstagioCidadao(cpf);
         return new ResponseEntity<>(estagio, HttpStatus.OK);
+    }
+
+    @PutMapping("")
+    public ResponseEntity<Cidadao> atualizaCidadao(@RequestBody UpdateCidadaoDto updateCidadaoDto, @RequestParam String cpf) throws CidadaoNaoEncontrado {
+        Cidadao cidadao = cidadaoService.update(cpf, updateCidadaoDto);
+        // atualizar para pegar informação de usuário logado da sessão
+        return new ResponseEntity<>(cidadao, HttpStatus.OK);
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<Cidadao> getCidadao(@RequestParam String cpf) throws CidadaoNaoEncontrado {
+        Cidadao cidadao = cidadaoService.findByCpf(cpf);
+        // atualizar para pegar informação de usuário logado da sessão
+        return new ResponseEntity<>(cidadao, HttpStatus.OK);
     }
 }
