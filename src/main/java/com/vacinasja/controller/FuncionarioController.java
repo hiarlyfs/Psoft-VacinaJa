71daa68b9a5113f4729ab29437952df4df0c825a
Pedro D <pedroxcg@gmail.com>
2021-05-12 20:09:49 -0300

    Alterado nome de erro para evitar incompatibilidades

--- src/main/java/com/vacinasja/controller/FuncionarioController.java
+++ src/main/java/com/vacinasja/controller/FuncionarioController.java
@@ -1,7 +1,7 @@
 package com.vacinasja.controller;
 
 import com.vacinasja.dto.funcionario.InsertFuncionarioDto;
-import com.vacinasja.error.cidadao_error.CidadaoNaoEncontrado;
+import com.vacinasja.error.cidadao_error.CidadaoNaoEncontradoCpf;
 import com.vacinasja.error.status_cadastro_error.StatusInvalido;
 import com.vacinasja.error.tipo_login_error.TipoLoginInvalido;
 import com.vacinasja.model.LoginFuncionario;
@@ -10,8 +10,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.Map;
 
 @RestController
 @RequestMapping("/funcionario")
@@ -23,7 +21,7 @@ public class FuncionarioController {
 
 
     @PostMapping("")
-    public ResponseEntity<LoginFuncionario> save(@RequestBody InsertFuncionarioDto insertFuncionarioDto) throws TipoLoginInvalido, StatusInvalido, CidadaoNaoEncontrado {
+    public ResponseEntity<LoginFuncionario> save(@RequestBody InsertFuncionarioDto insertFuncionarioDto) throws TipoLoginInvalido, StatusInvalido, CidadaoNaoEncontradoCpf {
         LoginFuncionario loginFuncionario = funcionarioService.save(insertFuncionarioDto);
 
         return new ResponseEntity<>(loginFuncionario, HttpStatus.OK);
--- src/main/java/com/vacinasja/service/funcionario_service/FuncionarioServiceImpl.java
+++ src/main/java/com/vacinasja/service/funcionario_service/FuncionarioServiceImpl.java
@@ -1,21 +1,17 @@
 package com.vacinasja.service.funcionario_service;
 
 import com.vacinasja.dto.funcionario.InsertFuncionarioDto;
-import com.vacinasja.error.cidadao_error.CidadaoNaoEncontrado;
+import com.vacinasja.error.cidadao_error.CidadaoNaoEncontradoCpf;
 import com.vacinasja.error.status_cadastro_error.StatusInvalido;
 import com.vacinasja.error.tipo_login_error.TipoLoginInvalido;
 import com.vacinasja.model.Cidadao;
 import com.vacinasja.model.Funcionario;
 import com.vacinasja.model.LoginFuncionario;
-import com.vacinasja.model.StatusCadastro;
 import com.vacinasja.repository.FuncionarioRepository;
 import com.vacinasja.service.cidadao_service.CidadaoService;
 import com.vacinasja.service.login_funcionario_service.LoginFuncionarioService;
-import com.vacinasja.service.status_cadastro_service.StatusCadastroService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.util.Map;
 
 @Service
 public class FuncionarioServiceImpl implements FuncionarioService{
@@ -30,7 +26,7 @@ public class FuncionarioServiceImpl implements FuncionarioService{
     FuncionarioRepository funcionarioRepository;
 
     @Override
-    public LoginFuncionario save(InsertFuncionarioDto insertFuncionarioDto) throws CidadaoNaoEncontrado, TipoLoginInvalido, StatusInvalido {
+    public LoginFuncionario save(InsertFuncionarioDto insertFuncionarioDto) throws CidadaoNaoEncontradoCpf, TipoLoginInvalido, StatusInvalido {
         Cidadao cidadao = cidadaoService.findByCpf(insertFuncionarioDto.getCpf());
         Funcionario funcionario = new Funcionario(cidadao, insertFuncionarioDto.getCargo(), insertFuncionarioDto.getLocalTrabalho());
         funcionarioRepository.save(funcionario);
