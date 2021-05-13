b17df6414f171ff47b569a55eeb12f22a85eb25b
lucasgsa <lucasgsalmeida19@gmail.com>
2021-05-13 00:00:44 -0300

    realizando publish do event

--- src/main/java/com/vacinasja/component/DayEventPublisher.java
+++ src/main/java/com/vacinasja/component/DayEventPublisher.java
@@ -1,8 +1,12 @@
 package com.vacinasja.component;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+
+import com.vacinasja.event.passou_dia.PassouDiaEvent;
 
 @Component
 @EnableScheduling
@@ -10,8 +14,11 @@ public class DayEventPublisher {
 	
 	private static final String TIME_ZONE = "America/Sao_Paulo";
 	
+	@Autowired
+	ApplicationEventPublisher applicationEventPublisher;
+	
 	@Scheduled(cron = "0 0 0 * * ?", zone = TIME_ZONE)
 	public void passouDia() {
-		System.out.println("passou um diaaa");
+		applicationEventPublisher.publishEvent(new PassouDiaEvent(this));
 	}
 }
