e742f43a5fb760f300bbec4d5e7210234514f059
italomiguelpinheiro <italo.pinheiro@ccc.ufcg.edu.br>
2021-05-13 11:53:30 -0300

    feature/registrar

--- src/main/java/com/vacinasja/controller/CidadaoController.java
+++ src/main/java/com/vacinasja/controller/CidadaoController.java
@@ -2,6 +2,7 @@ package com.vacinasja.controller;
 
 import com.vacinasja.dto.cidadao.InsertCidadaoDto;
 import com.vacinasja.error.tipo_login_error.TipoLoginInvalido;
+import com.vacinasja.model.Cidadao;
 import com.vacinasja.model.LoginCidadao;
 import com.vacinasja.service.cidadao_service.CidadaoService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 @RestController
 @RequestMapping("/cidadao")
@@ -25,5 +29,37 @@ public class CidadaoController {
 
         return new ResponseEntity<>(loginCidadao, HttpStatus.OK);
     }
-
+    
+    
+  /*  
+    @PostMapping 
+    public ResponseEntity<List<Cidadao>> habilitarVacinacaoIdade(@RequestBody  Date idadeMinima){
+    	List<Cidadao> cidadaosHabilitados = new ArrayList<Cidadao>();
+    	
+    	
+    	
+    	return new ResponseEntity<>(cidadaosHabilitados, HttpStatus.OK);
+    }
+    
+    @PostMapping 
+    public ResponseEntity<List<Cidadao>> habilitarVacinacaoIdadeComorbidade(@RequestBody  String comorbidade){
+    	List<Cidadao> cidadaosHabilitados = new ArrayList<Cidadao>();
+    	
+    	
+    	
+    	return new ResponseEntity<>(cidadaosHabilitados, HttpStatus.OK);
+    }
+    
+    
+    @PostMapping 
+    public ResponseEntity<List<Cidadao>> habilitarVacinacaoProfissao(@RequestBody  String profissao){
+    	List<Cidadao> cidadaosHabilitados = new ArrayList<Cidadao>();
+    	
+    	
+    	
+    	return new ResponseEntity<>(cidadaosHabilitados, HttpStatus.OK);
+    }
+*/
+    
+    
 }
