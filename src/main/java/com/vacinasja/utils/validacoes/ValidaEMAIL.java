f6585cea435188caefa0bf423e02ee933795588a
Pedro D <pedroxcg@gmail.com>
2021-05-16 13:38:04 -0300

    Alterado validação de email agora usa regex

--- src/main/java/com/vacinasja/utils/validacoes/ValidaEMAIL.java
+++ src/main/java/com/vacinasja/utils/validacoes/ValidaEMAIL.java
@@ -2,10 +2,19 @@ package com.vacinasja.utils.validacoes;
 
 import com.vacinasja.error.verificacoes_error.EmailInvalido;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class ValidaEMAIL {
+    private static final String EMAIL_PATTERN =
+            "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
+                    + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+
+    private static final Pattern pattern = Pattern.compile(EMAIL_PATTERN, Pattern.CASE_INSENSITIVE);
 
     public static void validaEMAIL(String email) throws EmailInvalido {
-        if (email.isEmpty() || !(email.contains("@")) || !(email.contains("."))) throw new EmailInvalido(email);
+        Matcher matcher = pattern.matcher(email);
+        if (!matcher.matches()) throw new EmailInvalido(email);
     }
 
 }
